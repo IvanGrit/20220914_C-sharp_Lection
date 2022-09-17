f5da2eeb8e3fc3800a1f83b0b766d1b581b23130
Ivan Gr <of_2000@mail.ru>
2022-09-18 00:30:15 +0400

    008 Dog

---
+++ 008_Dog/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Hello, please input distance between friends: ");
+int distance = Console.Read();
+int firstFriendSpeed = 1;
+int secondFriendSpeed = 2;
+int dogSpeed = 20;
+int friend = 2;
+int count = 0;
+int DogTime = 0;
+while(distance > 10)
+{
+    if(friend == 1)
+    {
+        DogTime = distance / (firstFriendSpeed + dogSpeed);
+        friend = 2;
+    }
+    else
+    {
+        DogTime = distance / (secondFriendSpeed + dogSpeed);
+        friend = 1;
+    }
+    distance = distance - (firstFriendSpeed + secondFriendSpeed) * DogTime;
+    count++;
+}
+
+Console.Write("Dog will run between friends ");
+Console.Write(count);
+Console.WriteLine(" times!");
