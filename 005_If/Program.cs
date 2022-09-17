6f7a7717da18f34390e2562bb6bdc9a6c40b3d80
Ivan Gr <of_2000@mail.ru>
2022-09-17 19:27:57 +0400

    005 If

---
+++ 005_If/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Hello, please input your name: ");
+string uzerName = Console.ReadLine();
+
+if(uzerName.ToLower() == "mary")
+{
+    Console.WriteLine("Great! Mary come here!!!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(uzerName);
+}
+
+
