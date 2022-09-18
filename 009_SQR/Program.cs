2a3b71d0cc6aa239fa56b380c6086a9faaac235e
Ivan Gr <of_2000@mail.ru>
2022-09-18 16:58:44 +0400

    009 SQR

---
+++ 009_SQR/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("Input Number #1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input Number #2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int sqr1 = a*a;
+int sqr2 = Convert.ToInt32(Math.Pow(b,2));
+Console.WriteLine($"Квадрат числа {a} равен {sqr1}");
+Console.WriteLine($"Квадрат числа {b} равен {sqr2}");
